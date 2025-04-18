9c66f1c380f507577bb2ef74e3b336e791e64fbe
Md. Rifatul Islam <rifatuul@gmail.com>
2025-04-19 02:12:32 +0600

    inset data using OnModelCreating

--- DbOperationsWithEfCoreApp/DbOperationsWithEfCoreApp/Data/AppDbContext.cs
+++ DbOperationsWithEfCoreApp/DbOperationsWithEfCoreApp/Data/AppDbContext.cs
@@ -8,5 +8,27 @@ namespace DbOperationsWithEfCoreApp.Data
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Currency>().HasData(
+                new Currency() { Id = 1, Title = "BD", Description = "Bangladeshi Taka" },
+                new Currency() { Id = 2, Title = "Dollar", Description = "Dollar" },
+                new Currency() { Id = 3, Title = "Euro", Description = "Euro" },
+                new Currency() { Id = 4, Title = "Dinar", Description = "Dinar" }
+                );
+
+            modelBuilder.Entity<Language>().HasData(
+    new Language() { Id = 1, Title = "Bangla", Description = "Bangla" },
+    new Language() { Id = 2, Title = "English", Description = "English" },
+    new Language() { Id = 3, Title = "Chinis", Description = "Chinis" },
+    new Language() { Id = 4, Title = "Turky", Description = "Turky" }
+    );
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Currency> Currencies { get; set; }
+        public DbSet<BookPrice> BookPrices { get; set; }
     }
 }
